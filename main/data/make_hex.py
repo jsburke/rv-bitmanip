8bfd7adca1137255bf28e47014d77efd9017e04e
jsburke <jburke@draper.com>
2018-10-14 22:01:57 -0400

    read in mode and entries

--- main/data/make_hex.py
+++ main/data/make_hex.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+#  make_hex.py usage
+#
+#    make_hex is a script meant to generate hex files for use in
+#    hardware unit testing
+#
+#  command line options
+#
+#  -h,  --help       display this message (default option)
+#
+#  -32, --rv32       generate hex files with 32 bit hex numbers [default]
+#  -64, --rv64       generate hex files with 64 bit hex numbers
+#
+#  -e,  --entries    defines number of integers in each hex file [default 16]
+#
 
 #############################
 ##                         ##
@@ -7,7 +21,7 @@
 #############################
 
 import random as rd
-import sys, argparse
+import os, sys, argparse
 
 #############################
 ##                         ##
@@ -27,10 +41,10 @@ def randHexDigit():
 class parserDefinedError(argparse.ArgumentParser):
   def error(self, msg = ""):                        # use a block comment at top as
     if (msg): print("\n  ERROR: %s\n" % msg)        # info to print to command line
-    source = open(sys.argv[0])
-    for (lineNo, line) in enumerate(file):
+    source = open(sys.argv[0], "r")
+    for (lineNo, line) in enumerate(source):
       if(line[0] != "#"): sys.exit(msg != "")       # exit on printing first non comment line
-      if(lineNo > 0): print(line[1:].rstrip("\n"))  # print all info lines not the shebang
+      if(lineNo > 0): print(line[1:].rstrip("\n"))  # print all info lines not the shebang, start 2
 
 def parseCMD():
   parser = parserDefinedError(add_help = False)
@@ -65,5 +79,10 @@ def parseCMD():
 
 def main():
   options = parseCMD()
+
+  entries = options.entries
+
+  mode = "32"
+  if(options.rv64): mode = "64"
 
 main()
