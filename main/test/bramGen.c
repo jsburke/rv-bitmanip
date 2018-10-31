54e2cc586dfc94af813b2a264145dd09d72b6874
jsburke <jburke@draper.com>
2018-10-30 22:24:27 -0400

    prove corner cases work in 322 and 64 bit

--- main/test/bramGen.c
+++ main/test/bramGen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 #include "bitmanip.h"
 
@@ -26,12 +27,12 @@ void xlen_corner_cases(xlen_t *arr, int start){
   arr[start++] =    1ULL << (XLEN - 1);  // MSB 
   arr[start++] = ~(1ULL << (XLEN - 1));  // MSB unset
 
-  int shifts = (XLEN == 32) ? 4 : 8;
-  for(int i = 0; i < shifts; i++){ // alternating
-    arr[start]      +=   5;        // zeros and ones
+  int shifts = (XLEN == 32) ? 8 : 16;
+  for(int i = 0; i < shifts; i++){ // alternating zeros and ones 
+    arr[start]      <<=  4;    // shift up by nibble
+    arr[start + 1]  <<=  4;    // can be done faster but meh
+    arr[start]      +=   5;        
     arr[start + 1]  += 0xA;
-    arr[start]     <<= 1;
-    arr[start + 1] <<= 1;
   }
 }
 
@@ -40,9 +41,34 @@ void xlen_arr_fill(FILE *urand, xlen_t *arr, int start, int len){
     arr[i] = rand_val(urand);
 }
 
-void main(){
+///////////////////////////////////////
+//                                   //
+//  Meat                             //
+//                                   //
+///////////////////////////////////////
 
-  FILE *urand = rand_init();
-  printf("random value %" PR_HEX "\n", rand_val(urand));
+int main(int argc, char *argv[]){
 
+  if(argc != 2){
+    printf("*******  ERROR  *******\n");
+    printf("  bramGen requires one \n");
+    printf("  argument to know how \n");
+    printf("  many entries to create\n\n");
+    return 1;
+  }
+
+  long no_entries = strtol(argv[1], NULL, 10);
+  no_entries = (no_entries > 8) ? no_entries : 8;  //cover corner cases at least
+
+  xlen_t rs1[no_entries];
+//  xlen_t rs2[no_entries];
+
+  xlen_corner_cases(rs1, 0);
+
+  printf("rs1 values:\n\n");
+  for(int i = 0; i < 8; i++) printf("    rs1[%d] : %" PR_HEX "\n", i, rs1[i]);
+
+//  FILE *urand = rand_init();
+
+  return 0;
 }
