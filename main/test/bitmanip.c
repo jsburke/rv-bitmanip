37c79559b06f96dcad934e7f57f1220c45a4d0a4
jsburke <jburke@draper.com>
2018-11-03 17:07:06 -0400

    move generic static fn out of preproc blocks

--- main/test/bitmanip.c
+++ main/test/bitmanip.c
@@ -46,6 +46,13 @@ xlen_t ror(xlen_t rs1, xlen_t rs2){
   return (rs1 >> shamt) | (rs1 << ((XLEN - shamt) & (XLEN - 1)));
 }
 
+// static fn for bit width sensitive shuffle instructions
+static xlen_t shuffle_stage(xlen_t src, xlen_t maskL, xlen_t maskR, int N){
+  xlen_t x = src & ~(maskL | maskR);
+  x |= ((src << N) & maskL) | ((src >> N) & maskR);
+  return x;
+}
+
 #ifdef RV32
 
 xlen_t grev(xlen_t rs1, xlen_t rs2){
@@ -56,12 +63,6 @@ xlen_t grev(xlen_t rs1, xlen_t rs2){
   if(shamt &  4) x = ((x & 0x0F0F0F0F) <<  4) | ((x & 0xF0F0F0F0) >>  4);
   if(shamt &  8) x = ((x & 0x00FF00FF) <<  8) | ((x & 0xFF00FF00) >>  8);
   if(shamt & 16) x = ((x & 0x0000FFFF) << 16) | ((x & 0xFFFF0000) >> 16);
-  return x;
-}
-
-static xlen_t shuffle_stage(xlen_t src, xlen_t maskL, xlen_t maskR, int N){
-  xlen_t x = src & ~(maskL | maskR);
-  x |= ((src << N) & maskL) | ((src >> N) & maskR);
   return x;
 }
 
@@ -94,12 +95,6 @@ xlen_t grev(xlen_t rs1, xlen_t rs2){
   if(shamt &  8)  x = ((x & 0x00FF00FF00FF00FFLL) <<   8) | ((x & 0xFF00FF00FF00FF00LL) >>   8);
   if(shamt & 16)  x = ((x & 0x0000FFFF0000FFFFLL) <<  16) | ((x & 0xFFFF0000FFFF0000LL) >>  16);
   if(shamt & 32)  x = ((x & 0x00000000FFFFFFFFLL) <<  32) | ((x & 0xFFFFFFFF00000000LL) >>  32);
-  return x;
-}
-
-static xlen_t shuffle_stage(xlen_t src, xlen_t maskL, xlen_t maskR, int N){
-  xlen_t x = src & ~(maskL | maskR);
-  x |= ((src << N) & maskL) | ((src >> N) & maskR);
   return x;
 }
 
