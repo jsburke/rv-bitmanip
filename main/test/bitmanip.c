ffe072123692a10255433abb5489bb11fb48310b
jsburke <jburke@draper.com>
2018-11-03 16:04:40 -0400

    move grev to match earlier section

--- main/test/bitmanip.c
+++ main/test/bitmanip.c
@@ -85,6 +85,18 @@ xlen_t unshfl(xlen_t rs1, xlen_t rs2){
 
 #elif RV64
 
+xlen_t grev(xlen_t rs1, xlen_t rs2){
+  xlen_t x = rs1;
+  int shamt  = rs2 & 63;
+  if(shamt &  1)  x = ((x & 0x5555555555555555LL) <<   1) | ((x & 0xAAAAAAAAAAAAAAAALL) >>   1);
+  if(shamt &  2)  x = ((x & 0x3333333333333333LL) <<   2) | ((x & 0xCCCCCCCCCCCCCCCCLL) >>   2);
+  if(shamt &  4)  x = ((x & 0xF0F0F0F0F0F0F0F0LL) <<   4) | ((x & 0x0F0F0F0F0F0F0F0FLL) >>   4);
+  if(shamt &  8)  x = ((x & 0x00FF00FF00FF00FFLL) <<   8) | ((x & 0xFF00FF00FF00FF00LL) >>   8);
+  if(shamt & 16)  x = ((x & 0x0000FFFF0000FFFFLL) <<  16) | ((x & 0xFFFF0000FFFF0000LL) >>  16);
+  if(shamt & 32)  x = ((x & 0x00000000FFFFFFFFLL) <<  32) | ((x & 0xFFFFFFFF00000000LL) >>  32);
+  return x;
+}
+
 static xlen_t shuffle_stage(xlen_t src, xlen_t maskL, xlen_t maskR, int N){
   xlen_t x = src & ~(maskL | maskR);
   x |= ((src << N) & maskL) | ((src >> N) & maskR);
@@ -108,18 +120,6 @@ xlen_t unshfl(xlen_t rs1, xlen_t rs2){
   if(rs2 &  4) x = shuffle_stage(x, 0x0F000F000F000F00LL, 0x00F000F000F000F0LL,  4);
   if(rs2 &  8) x = shuffle_stage(x, 0x00FF000000FF0000LL, 0x0000FF000000FF00LL,  8);
   if(rs2 & 16) x = shuffle_stage(x, 0x0000FFFF00000000LL, 0x00000000FFFF0000LL, 16);
-  return x;
-}
-
-xlen_t grev(xlen_t rs1, xlen_t rs2){
-  xlen_t x = rs1;
-  int shamt  = rs2 & 63;
-  if(shamt &  1)  x = ((x & 0x5555555555555555LL) <<   1) | ((x & 0xAAAAAAAAAAAAAAAALL) >>   1);
-  if(shamt &  2)  x = ((x & 0x3333333333333333LL) <<   2) | ((x & 0xCCCCCCCCCCCCCCCCLL) >>   2);
-  if(shamt &  4)  x = ((x & 0xF0F0F0F0F0F0F0F0LL) <<   4) | ((x & 0x0F0F0F0F0F0F0F0FLL) >>   4);
-  if(shamt &  8)  x = ((x & 0x00FF00FF00FF00FFLL) <<   8) | ((x & 0xFF00FF00FF00FF00LL) >>   8);
-  if(shamt & 16)  x = ((x & 0x0000FFFF0000FFFFLL) <<  16) | ((x & 0xFFFF0000FFFF0000LL) >>  16);
-  if(shamt & 32)  x = ((x & 0x00000000FFFFFFFFLL) <<  32) | ((x & 0xFFFFFFFF00000000LL) >>  32);
   return x;
 }
 
