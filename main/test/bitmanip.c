c6565c97193709aaa9f8ffe4f587477230268a8f
jsburke <jburke@draper.com>
2018-11-04 07:23:21 -0500

    fix swapped masks on lhs and rhs of OR

--- main/test/bitmanip.c
+++ main/test/bitmanip.c
@@ -91,7 +91,7 @@ xlen_t grev(xlen_t rs1, xlen_t rs2){
   int shamt  = rs2 & 63;
   if(shamt &  1)  x = ((x & 0x5555555555555555LL) <<   1) | ((x & 0xAAAAAAAAAAAAAAAALL) >>   1);
   if(shamt &  2)  x = ((x & 0x3333333333333333LL) <<   2) | ((x & 0xCCCCCCCCCCCCCCCCLL) >>   2);
-  if(shamt &  4)  x = ((x & 0xF0F0F0F0F0F0F0F0LL) <<   4) | ((x & 0x0F0F0F0F0F0F0F0FLL) >>   4);
+  if(shamt &  4)  x = ((x & 0x0F0F0F0F0F0F0F0FLL) <<   4) | ((x & 0xF0F0F0F0F0F0F0F0LL) >>   4);
   if(shamt &  8)  x = ((x & 0x00FF00FF00FF00FFLL) <<   8) | ((x & 0xFF00FF00FF00FF00LL) >>   8);
   if(shamt & 16)  x = ((x & 0x0000FFFF0000FFFFLL) <<  16) | ((x & 0xFFFF0000FFFF0000LL) >>  16);
   if(shamt & 32)  x = ((x & 0x00000000FFFFFFFFLL) <<  32) | ((x & 0xFFFFFFFF00000000LL) >>  32);
